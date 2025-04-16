741e14d0cd4eed865182830a10a28af810f5d3fa
Matt Young <matt.young@monetate.com>
2025-04-16 16:58:56 -0700

    end of part2

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Todo
+
+admin.site.register(Todo)
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'created_at', 'completed')
+    readonly_fields = ('created_at',)
+    ordering = ('created_at',)
--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    created_at = models.DateTimeField(default=datetime.now)
+    completed = models.BooleanField(default=False)
+    def __str__(self):
+        return self.title
