71de5b9fda5aa307dc4bdd29468aaa93ec85de49
Matt Young <matt.young@monetate.com>
2025-04-20 10:20:43 -0700

    form to update completed

--- tasks/urls.py
+++ tasks/urls.py
@@ -6,4 +6,5 @@ app_name = "tasks"
 urlpatterns = [
     path("", views.index, name="index"),
     path("<int:task_id>/", views.detail, name="detail"),
+    path("update/<int:task_id>/", views.update, name="update"),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.db.models import F
 
 from .models import Todo
 # Create your views here.
@@ -21,3 +23,13 @@ def detail(request, task_id):
     return render(request, 'tasks/detail.html', context)
     #return HttpResponse(f"Task: {task.title}, Description: {task.description}, Created at: {task.created_at}, Completed: {task.completed}")
     #return render(request, 'tasks/detail.html', {'task': task})
+
+def update(request, task_id):
+    #return HttpResponse(f"You're updating task {task_id}.")
+    task = get_object_or_404(Todo, id=task_id)
+    if request.method == 'POST':
+        task.completed = request.POST.get('completed') == 'on'
+        task.save()
+        return HttpResponseRedirect(reverse('tasks:detail', args=(task.id,)))
+    else:
+        return HttpResponseRedirect(reverse('tasks:detail', args=(task.id,)))
